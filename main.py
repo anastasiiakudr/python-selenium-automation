f7834ad7ab2a88ac4b1917794df282a4a1b40505
Anastasiia <anastasiia@peters-mbp.mynetworksettings.com>
2024-04-10 17:37:17 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Open browser
+driver = webdriver.Chrome()
+time.sleep(5)
+
+# Open page
+driver.get("https://practicetestautomation.com/practice-test-login/")
+time.sleep(5)
+
+# Type username student into Username field
+username_locator = driver.find_element(By.ID, "username")
+username_locator.send_keys("student")
+
+# Type password Password123 into Password field
+password_locator = driver.find_element(By.NAME, "password")
+password_locator.send_keys("Password123")
+
+# Push Submit button
+submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+submit_button_locator.click()
+time.sleep(2)
+
+# Verify new page URL contains practicetestautomation.com/logged-in-successfully/
+actual_url = driver.current_url
+assert actual_url == "https://practicetestautomation.com/logged-in-successfully/"
+
+# Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+text_locator = driver.find_element(By.TAG_NAME, "h1")
+actual_text = text_locator.text
+assert actual_text == "Logged In Successfully"
+
+# Verify button Log out is displayed on the new page
+button_log_out = driver.find_element(By.LINK_TEXT, "Log out")
+assert button_log_out.is_displayed()
---
+++ tests/test_login_page_negative.py
@@ -0,0 +1,33 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class TestsNegativeScenarios:
+    def test_login_page_negative(self):
+        # Open page
+        driver = webdriver.Chrome()
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+
+        # Type username incorrectUser into Username field
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys("incorrectUser")
+        time.sleep(2)
+
+        # Type password Password123 into Password field
+        password_locator = driver.find_element(By.NAME, "password")
+        password_locator.send_keys("Password123")
+
+        # Push Submit button
+        submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+        submit_button_locator.click()
+        time.sleep(2)
+
+        # Verify error message is displayed
+        error_message_locator = driver.find_element(By.ID, "error")
+        assert error_message_locator.is_displayed(), "Error message is not displayed, but it should be"
+
+        # Verify error message text is Your username is invalid!
+        error_message = error_message_locator.text
+        assert error_message == "Your username is invalid!", "Error message is not exacted"
