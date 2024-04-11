097d8fe4df722866d90a45274a4e5f99092d0421
anastasiiakudr <ankud.qa@gmail.com>
2024-04-10 18:30:54 -0700

    Modified negative test

--- tests/test_login_page_negative.py
+++ tests/test_login_page_negative.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 class TestsNegativeScenarios:
     @pytest.mark.login
     @pytest.mark.negative
-    def test_login_page_negative(self):
+    def test_negative_username(self):
         # Open page
         driver = webdriver.Chrome()
         driver.get("https://practicetestautomation.com/practice-test-login/")
@@ -34,3 +34,32 @@ class TestsNegativeScenarios:
         # Verify error message text is Your username is invalid!
         error_message = error_message_locator.text
         assert error_message == "Your username is invalid!", "Error message is not exacted"
+
+    @pytest.mark.login
+    @pytest.mark.negative
+    def test_negative_password(self):
+        # Open page
+        driver = webdriver.Chrome()
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+
+        # Type username student into Username field
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys("student")
+        time.sleep(2)
+
+        # Type password incorrectPassword into Password field
+        password_locator = driver.find_element(By.NAME, "password")
+        password_locator.send_keys("incorrectPassword")
+
+        # Push Submit button
+        submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+        submit_button_locator.click()
+        time.sleep(2)
+
+        # Verify error message is displayed
+        error_message_locator = driver.find_element(By.ID, "error")
+        assert error_message_locator.is_displayed(), "Error message is not displayed, but it should be"
+
+        # Verify error message text is Your password is invalid!
+        error_message = error_message_locator.text
+        assert error_message == "Your password is invalid!", "Error message is not exacted"
