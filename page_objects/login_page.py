5998e94f60532f294885d64678ae2a7109342a7c
anastasiiakudr <ankud.qa@gmail.com>
2024-06-21 15:01:00 -0700

    Created base_page.py and modified login_page.py.

--- page_objects/login_page.py
+++ page_objects/login_page.py
@@ -1,29 +1,23 @@
-from selenium.webdriver.support import expected_conditions as EC
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.support.wait import WebDriverWait
+from page_objects.base_page import BasePage
 
 
-class LoginPage:
-
+class LoginPage(BasePage):
     __url = "https://practicetestautomation.com/practice-test-login/"
     __username_field = (By.ID, "username")
     __password_field = (By.NAME, "password")
     __submit_button = (By.XPATH, "//button[@class='btn']")
 
     def __init__(self, driver: WebDriver):
-        self._driver = driver
+        super().__init__(driver)
 
     def open(self):
-        self._driver.get()
+        """Open the login page using the URL specified."""
+        self.open_url(self.__url)  # Call the method directly from BasePage
 
     def execute_login(self, username: str, password: str):
-        wait = WebDriverWait(self._driver, 10)
-        wait.until(EC.presence_of_element_located(self.__username_field))
-        self._driver.find_element(self.__username_field).send_keys(username)
-        wait.until(EC.presence_of_element_located(self.__password_field))
-        self._driver.find_element(self.__password_field).send_keys(password)
-        wait.until(EC.presence_of_element_located(self.__submit_button))
-        self._driver.find_element(self.__submit_button).click()
-
+        """Log in using the provided username and password."""
+        self._type(self.__username_field, username)
+        self._type(self.__password_field, password)
+        self._click(self.__submit_button)
